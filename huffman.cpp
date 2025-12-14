b6772f8af578ab864d196ca4330ef4c4b678a1ad
zajcevi0111-ai <zajcevi010@mail.ru>
2025-12-14 20:27:51 +0700

    Финальная версия лабораторной работы по алгоритму Хаффмана

--- huffman.cpp
+++ huffman.cpp
@@ -1,4 +1,5 @@
 ﻿#include <iostream>
+#include <fstream>
 #include <string>
 #include <map>
 #include <vector>
@@ -6,6 +7,7 @@
 #include <queue>
 #include <cmath>
 #include <iomanip>
+#include <bitset>
 using namespace std;
 
 struct Uzel {
@@ -13,19 +15,26 @@ struct Uzel {
     int chastota;
     Uzel* levyj;
     Uzel* pravyj;
+
+    Uzel(char s, int f) : simvol(s), chastota(f), levyj(nullptr), pravyj(nullptr) {}
+    Uzel(int f, Uzel* l, Uzel* r) : simvol('\0'), chastota(f), levyj(l), pravyj(r) {}
 };
 
-bool sravnenie(Uzel* a, Uzel* b) {
-    return a->chastota > b->chastota;
-}
+struct Sravnenie {
+    bool operator()(Uzel* a, Uzel* b) {
+        return a->chastota > b->chastota;
+    }
+};
 
 map<char, string> kodirovki;
+map<string, char> dekodirovki;
 
 void sozdatKody(Uzel* vershina, string kod) {
-    if (vershina == NULL) return;
+    if (!vershina) return;
 
-    if (vershina->simvol != 0) {
+    if (vershina->simvol != '\0') {
         kodirovki[vershina->simvol] = kod;
+        dekodirovki[kod] = vershina->simvol;
         return;
     }
 
@@ -34,162 +43,209 @@ void sozdatKody(Uzel* vershina, string kod) {
 }
 
 void ochistitPamyat(Uzel* uzel) {
-    if (uzel == NULL) return;
+    if (!uzel) return;
     ochistitPamyat(uzel->levyj);
     ochistitPamyat(uzel->pravyj);
     delete uzel;
 }
 
-int main() {
-    cout << "==========================================" << endl;
-    cout << "Labaratornaya: Algoritm Hafmana" << endl;
-    cout << "==========================================" << endl << endl;
+void kodirovatFile(string inputFile, string outputFile) {
+    ifstream fin(inputFile);
+    if (!fin) {
+        cout << "Oshibka: ne udalos otkryt fail " << inputFile << endl;
+        return;
+    }
 
-    string tekst = "KOV.KOROVA";
+    string text((istreambuf_iterator<char>(fin)), istreambuf_iterator<char>());
+    fin.close();
 
-    cout << "Tekst: " << tekst << endl;
-    cout << "Dlina: " << tekst.size() << " simvolov" << endl << endl;
+    if (text.empty()) {
+        cout << "Fail pustoy!" << endl;
+        return;
+    }
+
+    cout << "Kodiruem fail: " << inputFile << endl;
+    cout << "Dlina teksta: " << text.length() << " simvolov" << endl;
 
     map<char, int> chastoty;
-    for (int i = 0; i < tekst.length(); i++) {
-        chastoty[tekst[i]]++;
+    for (char c : text) {
+        chastoty[c]++;
     }
 
-    cout << "1. Chastoty simvolov:" << endl;
-    cout << "---------------------" << endl;
+    priority_queue<Uzel*, vector<Uzel*>, Sravnenie> ochered;
 
-    int vsego = tekst.length();
-    for (auto it = chastoty.begin(); it != chastoty.end(); it++) {
-        double veroyatnost = (double)it->second / vsego;
-        cout << "'" << it->first << "' - " << it->second
-            << " raz (" << fixed << setprecision(2) << veroyatnost * 100 << "%)" << endl;
-    }
-    cout << endl;
-
-    priority_queue<Uzel*, vector<Uzel*>, decltype(&sravnenie)> ochered(sravnenie);
-
-    for (auto para : chastoty) {
-        Uzel* novyj = new Uzel;
-        novyj->simvol = para.first;
-        novyj->chastota = para.second;
-        novyj->levyj = NULL;
-        novyj->pravyj = NULL;
-        ochered.push(novyj);
+    for (auto& para : chastoty) {
+        ochered.push(new Uzel(para.first, para.second));
     }
 
-    cout << "2. Stroim derevo:" << endl;
-    cout << "-----------------" << endl;
-
-    int shag = 1;
     while (ochered.size() > 1) {
         Uzel* levyj = ochered.top(); ochered.pop();
         Uzel* pravyj = ochered.top(); ochered.pop();
-
-        Uzel* roditel = new Uzel;
-        roditel->simvol = 0;
-        roditel->chastota = levyj->chastota + pravyj->chastota;
-        roditel->levyj = levyj;
-        roditel->pravyj = pravyj;
-
-        ochered.push(roditel);
-
-        cout << "Shag " << shag << ": " << levyj->chastota << "+"
-            << pravyj->chastota << "=" << roditel->chastota << endl;
-        shag++;
+        ochered.push(new Uzel(levyj->chastota + pravyj->chastota, levyj, pravyj));
     }
 
     Uzel* koren = ochered.top();
-    cout << "Gotovo! Koren: " << koren->chastota << endl << endl;
 
+    kodirovki.clear();
+    dekodirovki.clear();
     sozdatKody(koren, "");
 
-    cout << "3. Tablica kodov:" << endl;
-    cout << "-----------------" << endl;
+    string zakodirovanniyText = "";
+    for (char c : text) {
+        zakodirovanniyText += kodirovki[c];
+    }
+
+    ofstream fout(outputFile, ios::binary);
+    if (!fout) {
+        cout << "Oshibka: ne udalos sozdat fail " << outputFile << endl;
+        return;
+    }
+
+    fout << "HUFFMAN" << endl;
+    fout << text.length() << endl;
 
     for (auto& para : kodirovki) {
-        cout << "'" << para.first << "' -> " << para.second << endl;
+        fout << para.first << " " << para.second << endl;
     }
-    cout << endl;
+    fout << "CODED" << endl;
 
-    cout << "4. Szhatie:" << endl;
-    cout << "-----------" << endl;
+    int bitCount = zakodirovanniyText.length();
+    fout << bitCount << endl;
 
-    int bylo = vsego * 8;
-    int stalo = 0;
-
-    for (int i = 0; i < tekst.length(); i++) {
-        char c = tekst[i];
-        stalo += kodirovki[c].length();
-    }
-
-    cout << "Bylo: " << bylo << " bit" << endl;
-    cout << "Stalo: " << stalo << " bit" << endl;
-
-    double koefficient = (double)bylo / stalo;
-    cout << "V " << fixed << setprecision(1) << koefficient << " raza menshe" << endl << endl;
-
-    cout << "5. Teoriya (entropiya):" << endl;
-    cout << "-----------------------" << endl;
-
-    double H = 0;
-    for (auto& para : chastoty) {
-        double p = (double)para.second / vsego;
-        if (p > 0) {
-            H -= p * log(p) / log(2);
-        }
+    for (size_t i = 0; i < zakodirovanniyText.length(); i += 8) {
+        string byteStr = zakodirovanniyText.substr(i, 8);
+        while (byteStr.length() < 8) byteStr += "0";
+        bitset<8> bits(byteStr);
+        fout.put(static_cast<char>(bits.to_ulong()));
     }
 
-    double L = (double)stalo / vsego;
+    fout.close();
 
-    cout << "Entropiya H = " << fixed << setprecision(3) << H << endl;
-    cout << "Srednyaya dlina L = " << L << endl;
+    int originalSize = text.length() * 8;
+    int compressedSize = bitCount;
+    double compressionRatio = (double)originalSize / compressedSize;
 
-    if (H <= L && L <= H + 1) {
-        cout << "Teorema Shennona rabotaet" << endl;
-    }
-    cout << endl;
-
-    cout << "6. Primer kodirovaniya:" << endl;
-    cout << "-----------------------" << endl;
-
-    cout << tekst << " = ";
-    for (char c : tekst) {
-        cout << kodirovki[c] << " ";
-    }
-    cout << endl << endl;
-
-    cout << "7. Proverka:" << endl;
-    cout << "-----------" << endl;
-
-    bool prefiksnij = true;
-    vector<string> vseKody;
-    for (auto& para : kodirovki) {
-        vseKody.push_back(para.second);
-    }
-
-    for (int i = 0; i < vseKody.size(); i++) {
-        for (int j = i + 1; j < vseKody.size(); j++) {
-            if (vseKody[j].compare(0, vseKody[i].length(), vseKody[i]) == 0) {
-                prefiksnij = false;
-            }
-        }
-    }
-
-    cout << "Kod prefiksnij: " << (prefiksnij ? "da" : "net") << endl;
-
-    double summaKrafta = 0;
-    for (auto& kod : vseKody) {
-        summaKrafta += 1.0 / (1 << kod.length());
-    }
-
-    cout << "Summa Krafta: " << summaKrafta << (summaKrafta <= 1 ? " <= 1" : " > 1") << endl;
-    cout << endl;
+    cout << "\nRezultaty szhatiya:" << endl;
+    cout << "Ishodnyy razmer: " << originalSize << " bit" << endl;
+    cout << "Szhatyy razmer: " << compressedSize << " bit" << endl;
+    cout << "Koeffitsient szhatiya: " << fixed << setprecision(2) << compressionRatio << ":1" << endl;
+    cout << "Szhatyy fail sohranen kak: " << outputFile << endl;
 
     ochistitPamyat(koren);
+}
 
-    cout << "==========================================" << endl;
-    cout << "Konec" << endl;
-    cout << "==========================================" << endl;
+void dekodirovatFile(string inputFile, string outputFile) {
+    ifstream fin(inputFile, ios::binary);
+    if (!fin) {
+        cout << "Oshibka: ne udalos otkryt fail " << inputFile << endl;
+        return;
+    }
+
+    string magic;
+    fin >> magic;
+    if (magic != "HUFFMAN") {
+        cout << "Oshibka: eto ne huffman-file!" << endl;
+        return;
+    }
+
+    int originalLength;
+    fin >> originalLength;
+    fin.ignore();
+
+    kodirovki.clear();
+    dekodirovki.clear();
+
+    string line;
+    while (getline(fin, line) && line != "CODED") {
+        if (line.length() >= 3) {
+            char symbol = line[0];
+            string code = line.substr(2);
+            kodirovki[symbol] = code;
+            dekodirovki[code] = symbol;
+        }
+    }
+
+    int bitCount;
+    fin >> bitCount;
+    fin.ignore();
+
+    string zakodirovanniyBits = "";
+    char byte;
+    while (fin.get(byte)) {
+        bitset<8> bits(byte);
+        zakodirovanniyBits += bits.to_string();
+    }
+
+    zakodirovanniyBits = zakodirovanniyBits.substr(0, bitCount);
+
+    string decodedText = "";
+    string currentCode = "";
+
+    for (char bit : zakodirovanniyBits) {
+        currentCode += bit;
+        if (dekodirovki.find(currentCode) != dekodirovki.end()) {
+            decodedText += dekodirovki[currentCode];
+            currentCode = "";
+        }
+    }
+
+    decodedText = decodedText.substr(0, originalLength);
+
+    ofstream fout(outputFile);
+    if (!fout) {
+        cout << "Oshibka: ne udalos sozdat fail " << outputFile << endl;
+        return;
+    }
+
+    fout << decodedText;
+    fout.close();
+
+    cout << "Dekodirovanie vipolneno!" << endl;
+    cout << "Rasshifrovannyy text sohranen v: " << outputFile << endl;
+    cout << "Proverka: " << (decodedText.length() == originalLength ? "OK" : "ERROR") << endl;
+}
+
+void pokazatMenu() {
+    cout << "    ALGORITM HAFMANA - LABORATORNAYA" << endl;
+    cout << "1. Kodirovat fail" << endl;
+    cout << "2. Dekodirovat fail" << endl;
+    cout << "3. Vihod" << endl;
+    cout << "Viberite deystvie (1-3): ";
+}
+
+int main() {
+    int vibor;
+
+    do {
+        pokazatMenu();
+        cin >> vibor;
+        cin.ignore();
+
+        if (vibor == 1) {
+            string inputFile, outputFile;
+            cout << "Vvedite imya vhodnogo faila (txt): ";
+            getline(cin, inputFile);
+            cout << "Vvedite imya vihodnogo faila: ";
+            getline(cin, outputFile);
+            kodirovatFile(inputFile, outputFile);
+        }
+        else if (vibor == 2) {
+            string inputFile, outputFile;
+            cout << "Vvedite imya szhatogo faila: ";
+            getline(cin, inputFile);
+            cout << "Vvedite imya rasshifrovannogo faila: ";
+            getline(cin, outputFile);
+            dekodirovatFile(inputFile, outputFile);
+        }
+        else if (vibor == 3) {
+            cout << "Vihod..." << endl;
+        }
+        else {
+            cout << "Nekorrektniy vibor!" << endl;
+        }
+
+        cout << endl;
+    } while (vibor != 3);
 
     return 0;
 }
